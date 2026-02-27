ce48ffda668aaca37b6823b5fa3f5cb4f1758450
Valentina Niño <valeaguirre1603@gmail.com>
2026-02-26 21:09:33 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ def main():
     clean_data = transform_weather(raw_data)
     load_to_csv(clean_data)
 
-    print("Weather ETL pipeline executed successfully 🚀")
+    print("Weather ETL pipeline executed successfully 🚀!")
 
 if __name__ == "__main__":
     main()
