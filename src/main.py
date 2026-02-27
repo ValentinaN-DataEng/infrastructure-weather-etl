3a945b49a92bb8fc3036d4f85aed20354d014b2d
Valentina Niño <valeaguirre1603@gmail.com>
2026-02-26 20:19:29 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,16 @@
+from extract import extract_weather
+from transform import transform_weather
+from load import load_to_csv
+
 def main():
-    print("Project pipeline starting...")
+    latitude = 4.7110
+    longitude = -74.0721
+
+    raw_data = extract_weather(latitude, longitude)
+    clean_data = transform_weather(raw_data)
+    load_to_csv(clean_data)
+
+    print("ETL pipeline executed successfully.")
 
 if __name__ == "__main__":
     main()
