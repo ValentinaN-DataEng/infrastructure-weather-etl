d8d09051862f842f51b22f324756a6e551117d02
Valentina Niño <valeaguirre1603@gmail.com>
2026-02-26 20:19:02 -0500

    Update load.py

--- src/load.py
+++ src/load.py
@@ -1,1 +1,13 @@
+import csv
+import os
 
+def load_to_csv(data, filename="data/weather_data.csv"):
+    file_exists = os.path.isfile(filename)
+
+    with open(filename, mode="a", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=data.keys())
+
+        if not file_exists:
+            writer.writeheader()
+
+        writer.writerow(data)
