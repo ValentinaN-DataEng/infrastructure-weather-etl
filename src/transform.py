43af593da08fddaeb209f95c3a74a2cfda61b16a
Valentina Niño <valeaguirre1603@gmail.com>
2026-02-26 20:18:31 -0500

    Update transform.py

--- src/transform.py
+++ src/transform.py
@@ -1,1 +1,9 @@
+def transform_weather(raw_data):
+    current = raw_data["current_weather"]
 
+    return {
+        "temperature": current["temperature"],
+        "windspeed": current["windspeed"],
+        "winddirection": current["winddirection"],
+        "time": current["time"]
+    }
